5429e138d4b97e60be4ed7815d29490b8bdeb188
endows <you@example.com>
2015-06-27 16:40:10 +0900

    ok

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp  = require('gulp')
-var shell = require('gulp-shell')
-var run = require('gulp-run');
-
-
-gulp.task('dev', function(){
-  run('webpack --watch')
-  run('live-server build')
-})
