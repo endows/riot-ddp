5860be83fde54638164adcf57a00b9114123d7cd
endows <you@example.com>
2015-06-27 17:37:40 +0900

    installed asteroid.browser

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 console.log('ok')
+var Asteroid = require('asteroid.browser')
+console.log(Asteroid)
+// var ceres = new Asteroid("localhost:3000");
+//
+// // Use real-time collections
+// var tasks = ceres.getCollection("posts");
+// window.add = function(){
+//   tasks.insert({
+//     description: "Date.now()"
+//   });
+// }
+//
+// window.TaskRQ = tasks.reactiveQuery({});
+//
